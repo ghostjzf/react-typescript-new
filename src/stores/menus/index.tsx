9cae9d88b3d921841cb6fb1e7757b847a18e8133
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-03-22 20:53:41 +0800

    fix: 修改菜单文件

--- src/stores/menus/index.tsx
+++ src/stores/menus/index.tsx
@@ -1,9 +1,27 @@
-import React, { Component } from "react";
+import { observable, action } from "mobx";
 
-class menus extends Component {
-  render() {
-    return <div>menus</div>;
+export class Menus {
+  allMenus: any[] = []; // 所有菜单项，主要用于菜单搜索。creatModule时将该模块下的菜单配置都push到该数组中
+
+  @observable menus = [];
+
+  @observable moduleId = "";
+
+  @action
+  setMenus(config) {
+    this.menus = config;
+  }
+
+  @action
+  setModuleId(id) {
+    this.moduleId = id ? "/" + id : "";
+  }
+
+  @observable searchValue = "";
+  @action
+  setSearchValue(value) {
+    this.searchValue = value;
   }
 }
 
-export default menus;
+export default new Menus();
