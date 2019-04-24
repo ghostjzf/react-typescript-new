b2c4f520a1557266437cd144346f37eb8814c8e4
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-04-24 17:36:52 +0800

    fix(error): antd按需加载样式无效的问题

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,9 @@
 process.on("unhandledRejection", err => {
-  throw err;
+    throw err;
 });
+
+process.env.NODE_ENV = "production";
+process.env.BABEL_ENV = "production";
 
 const webpack = require("webpack");
 const resolve = require("resolve");
@@ -18,159 +21,165 @@ const paths = require("./config/path.js");
 console.log(chalk.cyan("正在打包..."));
 
 module.exports = {
-  mode: "production",
-  devtool: "hidden-source-map",
-  entry: {
-    vendor: ["react", "react-dom", "react-router-dom", "mobx", "mobx-react"],
-    path: path.resolve(__dirname, "../src/index.tsx")
-  },
-  output: {
-    filename: "static/js/[name].[contenthash].js",
-    chunkFilename: "static/js/[name].[contenthash].js",
-    path: paths.appDist
-  },
-  module: {
-    rules: [
-      { parser: { requireEnsure: false } },
-      {
-        enforce: "pre",
-        test: /\.jsx?$/,
-        include: paths.appSrc,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.tsx?$/,
-        include: paths.appSrc,
-        loader: "ts-loader"
-      },
-      {
-        test: /\.css$/,
-        include: [paths.appSrc, `${paths.appNodeModules}/antd/es/`],
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.s[ac]ss$/,
-        exclude: /\.module\.s[ac]ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.(txt|htm)$/,
-        include: paths.appSrc,
-        loader: "raw-loader"
-      },
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: "file-loader",
-        options: {
-          name: "static/images/[name].[hash:8].[ext]"
-        }
-      },
-      {
-        test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)$/,
-        loader: "file-loader",
-        options: {
-          name: "static/media/[name].[hash:8].[ext]"
-        }
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
-      }
-    ]
-  },
-  resolve: {
-    alias: paths.alias,
-    extensions: [".wasm", ".mjs", ".tsx", ".ts", ".js", ".jsx", ".json"]
-  },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true // set to true if you want JS source maps
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ],
-    splitChunks: {
-      chunks: "async",
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: "~",
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        styles: {
-          name: "styles",
-          test: /\.css$/,
-          chunks: "all",
-          enforce: true
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
+    mode: "production",
+    devtool: "hidden-source-map",
+    entry: {
+        vendor: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+            "mobx",
+            "mobx-react"
+        ],
+        path: path.resolve(__dirname, "../src/index.tsx")
     },
-    runtimeChunk: "single"
-  },
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    module: "empty",
-    dgram: "empty",
-    dns: "mock",
-    fs: "empty",
-    net: "empty",
-    tls: "empty",
-    child_process: "empty"
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "static/css/[name].[hash].css",
-      chunkFilename: "static/css/[id].[hash].css"
-    }),
-    new HtmlWebpackPlugin({
-      title: "Hello React",
-      template: path.resolve(__dirname, "../public/index.html")
-    }),
-    new webpack.HashedModuleIdsPlugin(),
-    new ForkTsCheckerWebpackPlugin({
-      typescript: resolve.sync("typescript", {
-        basedir: paths.appNodeModules
-      }),
-      async: false,
-      checkSyntacticErrors: true,
-      tsconfig: paths.appTsConfig,
-      compilerOptions: {
-        module: "esnext",
-        moduleResolution: "node",
-        resolveJsonModule: true,
-        isolatedModules: true,
-        noEmit: true,
-        jsx: "preserve"
-      },
-      reportFiles: [
-        "**",
-        "!**/*.json",
-        "!**/__tests__/**",
-        "!**/?(*.)(spec|test).*",
-        "!**/src/setupProxy.*",
-        "!**/src/setupTests.*"
-      ],
-      watch: paths.appSrc,
-      silent: true,
-      formatter: typescriptFormatter
-    })
-  ],
-  performance: false
+    output: {
+        filename: "static/js/[name].[contenthash].js",
+        chunkFilename: "static/js/[name].[contenthash].js",
+        path: paths.appDist
+    },
+    module: {
+        rules: [
+            { parser: { requireEnsure: false } },
+            {
+                enforce: "pre",
+                test: /\.jsx?$/,
+                include: paths.appSrc,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.tsx?$/,
+                include: paths.appSrc,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                include: [paths.appSrc, `${paths.appNodeModules}/antd/es/`],
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                exclude: /\.module\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(txt|htm)$/,
+                include: paths.appSrc,
+                loader: "raw-loader"
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: "file-loader",
+                options: {
+                    name: "static/images/[name].[hash:8].[ext]"
+                }
+            },
+            {
+                test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)$/,
+                loader: "file-loader",
+                options: {
+                    name: "static/media/[name].[hash:8].[ext]"
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ["file-loader"]
+            }
+        ]
+    },
+    resolve: {
+        alias: paths.alias,
+        extensions: [".wasm", ".mjs", ".tsx", ".ts", ".js", ".jsx", ".json"]
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ],
+        splitChunks: {
+            chunks: "async",
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: "~",
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                styles: {
+                    name: "styles",
+                    test: /\.css$/,
+                    chunks: "all",
+                    enforce: true
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        },
+        runtimeChunk: "single"
+    },
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    node: {
+        module: "empty",
+        dgram: "empty",
+        dns: "mock",
+        fs: "empty",
+        net: "empty",
+        tls: "empty",
+        child_process: "empty"
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "static/css/[name].[hash].css",
+            chunkFilename: "static/css/[id].[hash].css"
+        }),
+        new HtmlWebpackPlugin({
+            title: "Hello React",
+            template: path.resolve(__dirname, "../public/index.html")
+        }),
+        new webpack.HashedModuleIdsPlugin(),
+        new ForkTsCheckerWebpackPlugin({
+            typescript: resolve.sync("typescript", {
+                basedir: paths.appNodeModules
+            }),
+            async: false,
+            checkSyntacticErrors: true,
+            tsconfig: paths.appTsConfig,
+            compilerOptions: {
+                module: "esnext",
+                moduleResolution: "node",
+                resolveJsonModule: true,
+                isolatedModules: true,
+                noEmit: true,
+                jsx: "preserve"
+            },
+            reportFiles: [
+                "**",
+                "!**/*.json",
+                "!**/__tests__/**",
+                "!**/?(*.)(spec|test).*",
+                "!**/src/setupProxy.*",
+                "!**/src/setupTests.*"
+            ],
+            watch: paths.appSrc,
+            silent: true,
+            formatter: typescriptFormatter
+        })
+    ],
+    performance: false
 };
--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,9 @@
 process.on("unhandledRejection", err => {
-  throw err;
+    throw err;
 });
+
+process.env.NODE_ENV = "development";
+process.env.BABEL_ENV = "development";
 
 const webpack = require("webpack");
 const resolve = require("resolve");
@@ -15,134 +18,148 @@ const paths = require("./config/path.js");
 console.log(chalk.cyan("正在启动环境..."));
 
 module.exports = {
-  mode: "development",
-  devtool: "cheap-module-eval-source-map",
-  entry: {
-    vendor: ["react", "react-dom", "react-router-dom", "mobx", "mobx-react"],
-    path: path.resolve(__dirname, "../src/index.tsx")
-  },
-  output: {
-    filename: "static/js/[name].[hash:8].js",
-    chunkFilename: "static/js/[name].[hash:8].js",
-    path: paths.appDist
-  },
-  module: {
-    rules: [
-      // Disable require.ensure as it's not a standard language feature.
-      { parser: { requireEnsure: false } },
-      {
-        enforce: "pre",
-        test: /\.jsx?$/,
-        include: paths.appSrc,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.tsx?$/,
-        include: paths.appSrc,
-        loader: "ts-loader"
-      },
-      {
-        test: /\.css$/,
-        include: [paths.appSrc, `${paths.appSrc}/antd/es/`],
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.s[ac]ss$/,
-        include: paths.appSrc,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.(txt|htm)$/,
-        include: paths.appSrc,
-        loader: "raw-loader"
-      },
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: "file-loader",
-        options: {
-          name: "static/images/[name].[hash:8].[ext]"
-        }
-      },
-      {
-        test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)$/,
-        loader: "file-loader",
-        options: {
-          name: "static/media/[name].[hash:8].[ext]"
-        }
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    open: false,
-    hot: true,
-    compress: true,
-    port: 9000,
-    quiet: true,
-    overlay: true // 编译出现错误时，将错误直接显示在页面上
-  },
-  resolve: {
-    alias: paths.alias,
-    extensions: [".wasm", ".mjs", ".ts", ".tsx", ".js", ".json"]
-  },
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    module: "empty",
-    dgram: "empty",
-    dns: "mock",
-    fs: "empty",
-    net: "empty",
-    tls: "empty",
-    child_process: "empty"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Hello React",
-      template: path.resolve(__dirname, "../public/index.html")
-    }),
-    new FriendlyErrorsWebpackPlugin({
-      compilationSuccessInfo: {
-        messages: [
-          `Local: ${0 ? "https" : "http"}://localhost:9000`,
-          `On Your Network: ${0 ? "https" : "http"}://localhost:9000`
+    mode: "development",
+    devtool: "cheap-module-eval-source-map",
+    entry: {
+        vendor: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+            "mobx",
+            "mobx-react"
+        ],
+        path: path.resolve(__dirname, "../src/index.tsx")
+    },
+    output: {
+        filename: "static/js/[name].[hash:8].js",
+        chunkFilename: "static/js/[name].[hash:8].js",
+        path: paths.appDist
+    },
+    module: {
+        rules: [
+            // Disable require.ensure as it's not a standard language feature.
+            { parser: { requireEnsure: false } },
+            {
+                enforce: "pre",
+                exclude: paths.appNodeModules,
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.tsx?$/,
+                include: paths.appSrc,
+                loader: ["babel-loader", "ts-loader"]
+            },
+            {
+                test: /\.css$/,
+                exclude: paths.appSrc,
+                use: [
+                    { loader: "style-loader" },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                include: paths.appSrc,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(txt|htm)$/,
+                include: paths.appSrc,
+                loader: "raw-loader"
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: "file-loader",
+                options: {
+                    name: "static/images/[name].[hash:8].[ext]"
+                }
+            },
+            {
+                test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)$/,
+                loader: "file-loader",
+                options: {
+                    name: "static/media/[name].[hash:8].[ext]"
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ["file-loader"]
+            }
         ]
-      },
-      onErrors: null,
-      clearConsole: true
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ForkTsCheckerWebpackPlugin({
-      typescript: resolve.sync("typescript", {
-        basedir: paths.appNodeModules
-      }),
-      async: false,
-      checkSyntacticErrors: true,
-      tsconfig: paths.appTsConfig,
-      compilerOptions: {
-        module: "esnext",
-        moduleResolution: "node",
-        resolveJsonModule: true,
-        isolatedModules: true,
-        noEmit: true,
-        jsx: "preserve"
-      },
-      reportFiles: [
-        "**",
-        "!**/*.json",
-        "!**/__tests__/**",
-        "!**/?(*.)(spec|test).*",
-        "!**/src/setupProxy.*",
-        "!**/src/setupTests.*"
-      ],
-      watch: paths.appSrc,
-      silent: true,
-      formatter: typescriptFormatter
-    })
-  ],
-  performance: false
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        open: false,
+        hot: true,
+        compress: true,
+        port: 9000,
+        quiet: true,
+        overlay: true // 编译出现错误时，将错误直接显示在页面上
+    },
+    resolve: {
+        alias: paths.alias,
+        extensions: [".wasm", ".mjs", ".ts", ".tsx", ".js", ".json"]
+    },
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    node: {
+        module: "empty",
+        dgram: "empty",
+        dns: "mock",
+        fs: "empty",
+        net: "empty",
+        tls: "empty",
+        child_process: "empty"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Hello React",
+            template: path.resolve(__dirname, "../public/index.html")
+        }),
+        new FriendlyErrorsWebpackPlugin({
+            compilationSuccessInfo: {
+                messages: [
+                    `Local: ${0 ? "https" : "http"}://localhost:9000`,
+                    `On Your Network: ${0 ? "https" : "http"}://localhost:9000`
+                ]
+            },
+            onErrors: null,
+            clearConsole: true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new ForkTsCheckerWebpackPlugin({
+            typescript: resolve.sync("typescript", {
+                basedir: paths.appNodeModules
+            }),
+            async: false,
+            checkSyntacticErrors: true,
+            tsconfig: paths.appTsConfig,
+            compilerOptions: {
+                module: "esnext",
+                moduleResolution: "node",
+                resolveJsonModule: true,
+                isolatedModules: true,
+                noEmit: true,
+                jsx: "preserve"
+            },
+            reportFiles: [
+                "**",
+                "!**/*.json",
+                "!**/__tests__/**",
+                "!**/?(*.)(spec|test).*",
+                "!**/src/setupProxy.*",
+                "!**/src/setupTests.*"
+            ],
+            watch: paths.appSrc,
+            silent: true,
+            formatter: typescriptFormatter
+        })
+    ],
+    performance: false
 };
