faa7aca839f97d87f88538885d72f69974caa9eb
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-06-03 20:00:31 +0800

    feat(readme): 修改README.md说明

--- src/modules/CommonToolsIntroduce/modules/Git/index.tsx
+++ src/modules/CommonToolsIntroduce/modules/Git/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 
 class Git extends Component {
   render() {
@@ -6,4 +7,4 @@ class Git extends Component {
   }
 }
 
-export default Git;
+export default hot(module)(Git);
