f75b892c5d8d693c7839f68fc02d608a8328db2a
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-06-03 17:23:55 +0800

    chore(word): 修改文字

--- src/modules/CommonToolsIntroduce/modules/Git/index.tsx
+++ src/modules/CommonToolsIntroduce/modules/Git/index.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Git extends Component {
   render() {
-    return <div style={{ color: "red" }}>git module</div>;
+    return <div style={{ color: "red" }}>git module2</div>;
   }
 }
 
