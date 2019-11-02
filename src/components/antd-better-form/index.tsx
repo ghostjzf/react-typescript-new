1761f2dcb1e49ebfa1718218f2bb3287d1f9611e
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-11-03 00:03:37 +0800

    chore(delete): remove unused file

--- src/components/antd-better-form/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Form } from './Form';
-export { default as FormItem } from './FormItem';
-export { default as withForm } from './withForm';
--- src/components/react-better-form/withForm.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const withForm = () => {
-  return <div>1111</div>;
-};
-
-export default withForm;
